1dcb7b00bf15b553cd9b8fac043324a492e5bb29
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-27 09:37:55 -0700

    commit tree 36d6ae80bc213fe3c6f7940db1b709d4d1084c5e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default()
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    
+    num / 2
+   
+end
+
+# halve(6)
+
+
+
+
+
